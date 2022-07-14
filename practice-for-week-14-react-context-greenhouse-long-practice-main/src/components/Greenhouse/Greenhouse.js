b70ec41818a1bf719c33e123e483067e67578a9a
alice886 <baconhill4tmr@gmail.com>
2022-07-14 13:55:43 -0700

    phase1 done

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -1,17 +1,24 @@
+import { useContext } from 'react';
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { ThemeContext } from '../../../src/context/ThemeContext'
+
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
-function Greenhouse() {
 
+function Greenhouse() {
+  const { themeName, setThemeName } = useContext(ThemeContext);
+  let image;
+  if (themeName === 'day') image = dayImage;
+  else image = nightImage;
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
+      <img className='greenhouse-img'
+        src={image}
+        alt='greenhouse'
       />
       <LightSwitch />
       <ClimateStats />
@@ -20,3 +27,5 @@ function Greenhouse() {
 }
 
 export default Greenhouse;
+
+// src={ themeName === 'day' ? dayImage : nightImage }
